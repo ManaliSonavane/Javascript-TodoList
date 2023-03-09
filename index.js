a611abfb5db9a8ac4508f670c4373dfa178ff4c0
Manali Sonavane <manalisonavane1@gmail.com>
2023-03-09 16:18:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-/*
-let addBtn = document.querySelector('#add');
-let delTodoBtn = document.querySelector('.todo-item > .delete'); 
-let markTodoBtn = document.querySelector('.todo-item > .mark');
-*/
-
 let addTodoForm = document.querySelector('.add-todo');
 
 let todoInput = document.querySelector('input');
 
 let listContainer = document.querySelector('.list-container');
-
-/*what if i enter value and press enter, click doesn't happen here //if button is not inside form
-  can work with any button
-  
-  addBtn.addEventListener('click', function(){});
-*/
 
 //ol is parent(listCntainer)
 listContainer.addEventListener('click', function(event){
@@ -33,65 +21,31 @@ listContainer.addEventListener('click', function(event){
       }
 });
 
-/*function createBtn(text){
-    const btn = document.createElement("button");
-    btn.textContent(text);
-    return btn;
-}*/
-
 function appendTodoItem(text){
      const li = document.createElement('li');
-     //pre-defined fn -- adds class todo-item to newly created li 
      li.classList.add('todo-item');
      li.textContent =text;
      listContainer.append(li);
-      
-     //adding delete btn
      
      const btn= document.createElement('button');
      btn.textContent= 'Delete';
      btn.classList.add('delete');
      li.appendChild(btn);
 
-     //adding complete btn
      const btn2= document.createElement('button');
      btn2.textContent= 'Mark as complete';
      btn2.classList.add('mark');
      li.appendChild(btn2);
-    
-
-     /*const btn = createBtn("Delete");
-     li.appendChild(btn);
-
-     const btn2 = createBtn("Mark as complete");
-     li.appendChild(btn2); */
-
 }
 
-
-
-
-//form (more readable/understandable)
-//restricted to forms
-
 addTodoForm.addEventListener('submit', function(event){
-    event.preventDefault();
-    //this -> current element -> i.e. form
-    //add input value in todo lost
 
     let value= todoInput.value;
-    //console.log(value);
 
     if (value !== ""){
         appendTodoItem(value);
-        //value clear in input field after adding to list
         todoInput.value="";
     }
-   
-   // this['todo-list'].value //current form's input
-   
-   
-
 });
 
 
